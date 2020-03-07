27f9cc3988a7131d92cf0ca835020ec1d3fccf04
Nicole J <43453127+nj-keng@users.noreply.github.com>
2020-03-07 15:18:18 -0800

    can now recognize file getting dropped to the page

--- js/server.js
+++ js/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //links the css to the index page
@@ -6,18 +6,24 @@ app.use("/css", express.static("../css"));
 app.use("/img", express.static("../img"));
 app.use("/js", express.static("../js"));
 
-app.get('/', function (req, res) {
-    res.sendFile("index.html", {
-        root: "../"
-    });
-})
+app.get("/", function(req, res) {
+  res.sendFile("index.html", {
+    root: "../"
+  });
+});
 
-app.use(function (req, res, next) {
-    res.status(404).send("Nothing there, 404.");
-})
+app.get("/convert", function(req, res) {
+  res.sendFile("convert.html", {
+    root: "../html/"
+  });
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send("Nothing there, 404.");
+});
 
 // RUN SERVER
 let port = 8000;
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!');
-})
+app.listen(port, function() {
+  console.log("Example app listening on port " + port + "!");
+});
